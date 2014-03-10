9c66f683f74d7aa6318e7e5645998a1175c6ef52
Olivier Acker <olivier@robotmotel.com>
2014-03-10 08:50:00 +0000

    Initial Commit

---
+++ Thrift/Thrift.m
@@ -0,0 +1,13 @@
+//
+//  Thrift.m
+//  Thrift
+//
+//  Created by Olivier Acker on 10/03/2014.
+//  Copyright (c) 2014 Cyberroadie. All rights reserved.
+//
+
+#import "Thrift.h"
+
+@implementation Thrift
+
+@end
